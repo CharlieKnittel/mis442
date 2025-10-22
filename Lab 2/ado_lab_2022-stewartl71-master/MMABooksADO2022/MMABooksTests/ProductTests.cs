42cb4de1da0f92f09212356bd18384a18d4ce83b
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-10-21 20:11:31 -0700

    Created test for Product constructors

--- Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
+++ Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MMABooksBusinessClasses;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,28 @@ namespace MMABooksTests
 {
     public class ProductTests
     {
+        private Product def;
+        private Product c;
+
+        [SetUp]
+        public void Setup()
+        {
+            def = new Product();
+            c = new Product("A1B", "Chemistry 101", 3, 10.99m);
+        }
+
+        [Test]
+        public void TestProductConstructor()
+        {
+            Assert.IsNotNull(def);
+            Assert.AreEqual(null, def.ProductCode);
+            Assert.AreEqual(null, def.Description);
+
+            Assert.IsNotNull(c);
+            Assert.AreEqual("A1B", c.ProductCode);
+            Assert.AreEqual("Chemistry 101", c.Description);
+            Assert.AreEqual(3, c.OnHandQuantity);
+            Assert.AreEqual(10.99m, c.UnitPrice);
+        }
     }
 }
