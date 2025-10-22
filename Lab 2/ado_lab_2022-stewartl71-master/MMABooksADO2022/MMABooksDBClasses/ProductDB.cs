09db3d2012445a318a0dcbde33d833466899929d
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-10-21 22:14:03 -0700

    Added GetProduct method.

--- Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksDBClasses/ProductDB.cs
+++ Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksDBClasses/ProductDB.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using MMABooksBusinessClasses;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace MMABooksDBClasses
 {
     public static class ProductDB
     {
+        public static Product GetProduct(string productCode)
+        {
+            MySqlConnection connection = MMABooksDB.GetConnection();
+            string selectStatement
+                = "SELECT ProductCode, Description, OnHandQuantity, UnitPrice "
+                + "FROM Products "
+                + "WHERE ProductCode = @ProductCode";
+            MySqlCommand selectCommand =
+                new MySqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@ProductCode", productCode);
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader prodReader =
+                    selectCommand.ExecuteReader(CommandBehavior.SingleRow);
+                if (prodReader.Read())
+                {
+                    Product product = new Product();
+                    product.ProductCode = prodReader["ProductCode"].ToString();
+                    product.Description = prodReader["Description"].ToString();
+                    product.OnHandQuantity = (int)prodReader["OnHandQuantity"];
+                    product.UnitPrice = (decimal)prodReader["UnitPrice"];
+                    return product;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
