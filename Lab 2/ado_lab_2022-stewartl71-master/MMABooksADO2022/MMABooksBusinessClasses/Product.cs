c5b7365f3f7686a0c9c8f77afd63b6cc172de029
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-10-21 19:51:27 -0700

    Implemented instance variables.

--- Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksBusinessClasses/Product.cs
+++ Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksBusinessClasses/Product.cs
@@ -6,5 +6,54 @@ namespace MMABooksBusinessClasses
 {
     public class Product
     {
+        public Product() { }
+
+        public Product(string productcode, string description, int onhandquantity, decimal unitprice) 
+        {
+            ProductCode = productcode;
+            Description = description;
+            OnHandQuantity = onhandquantity;
+            UnitPrice = unitprice;
+        }
+
+        //instance variables
+        private string productCode;
+        private string description;
+        private int onHandQuantity;
+        private decimal unitPrice;
+
+        public string ProductCode
+        {
+            get
+            {
+                return productCode; ;
+            }
+            set
+            {
+                if (value.Trim().Length > 0 && value.Trim().Length <= 50)
+                    productCode = value;
+                else
+                    throw new ArgumentOutOfRangeException("Must be at least one character and no more than 50 characters");
+            }
+        }
+
+        public string Description
+        { 
+            get 
+            { 
+                return description; 
+            } 
+            set 
+            {
+                if (value.Trim().Length > 0 && value.Trim().Length <= 50)
+                    productCode = value;
+                else
+                    throw new ArgumentOutOfRangeException("Must be at least one character and no more than 50 characters");
+            }
+        public int OnHandQuantity
+            { get { return onHandQuantity; }
+
+        public decimal UnitPrice
+            { get { return unitPrice; } }
     }
 }
