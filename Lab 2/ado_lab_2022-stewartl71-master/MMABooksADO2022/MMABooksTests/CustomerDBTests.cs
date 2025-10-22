f05514943b9c767313be4e39ec044e8d2c77a376
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-10-21 18:52:25 -0700

    Created unit test for DeleteCustomer.

--- Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerDBTests.cs
+++ Lab 2/ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerDBTests.cs
@@ -10,6 +10,11 @@ namespace MMABooksTests
 {
     public class CustomerDBTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
 
         [Test]
         public void TestGetCustomer()
@@ -31,6 +36,30 @@ namespace MMABooksTests
             int customerID = CustomerDB.AddCustomer(c);
             c = CustomerDB.GetCustomer(customerID);
             Assert.AreEqual("Mickey Mouse", c.Name);
+        }
+
+        [Test]
+        public void TestDeleteCustomer()
+        {
+            // Create and add a customer
+            Customer c = new Customer();
+            c.Name = "Donald Duck";
+            c.Address = "202 Main Street";
+            c.City = "Orlando";
+            c.State = "FL";
+            c.ZipCode = "20202";
+
+            int customerID = CustomerDB.AddCustomer(c);
+
+            // Retrieve it again so we have a complete Customer object
+            c = CustomerDB.GetCustomer(customerID);
+
+            // Delete the customer
+            CustomerDB.DeleteCustomer(c);
+
+            // Verify the customer no longer exists
+            Customer deletedCustomer = CustomerDB.GetCustomer(customerID);
+            Assert.IsNull(deletedCustomer, "Customer should be deleted but still exists.");
         }
     }
 }
