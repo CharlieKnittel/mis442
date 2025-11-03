ecbe6974d16c5bb88094164b8a70bbd1760943a6
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-11-03 12:58:46 -0800

    Created CustomerDB constructors

--- Lab 3/MMABooksFramework2022/MMABooksDB/CustomerDB.cs
+++ Lab 3/MMABooksFramework2022/MMABooksDB/CustomerDB.cs
@@ -19,9 +19,11 @@ using DBDbType = MySql.Data.MySqlClient.MySqlDbType;
 
 namespace MMABooksDB
 {
-    public class CustomerDB
+    public class CustomerDB : DBBase, IReadDB, IWriteDB
     {
-        /*
+        public CustomerDB() : base() { }
+        public CustomerDB(DBConnection cn) : base(cn) { }
+
         public IBaseProps Create(IBaseProps p)
         {
             int rowsAffected = 0;
@@ -32,12 +34,20 @@ namespace MMABooksDB
             command.CommandType = CommandType.StoredProcedure;
             command.Parameters.Add("custId", DBDbType.Int32);
             command.Parameters.Add("name_p", DBDbType.VarChar);
-            ... there are more parameters here
+            command.Parameters.Add("address_p", DBDbType.VarChar);
+            command.Parameters.Add("city_p", DBDbType.VarChar);
+            command.Parameters.Add("state_p", DBDbType.VarChar);
+            command.Parameters.Add("zipCode_p", DBDbType.VarChar);
+            //... there are more parameters here
             command.Parameters[0].Direction = ParameterDirection.Output;
             command.Parameters["name_p"].Value = props.Name;
-            ... and more values here
+            command.Parameters["address_p"].Value = props.Address;
+            command.Parameters["city_p"].Value = props.City;
+            command.Parameters["state_p"].Value = props.State;
+            command.Parameters["zipCode_p"].Value = props.ZipCode;
+            //... and more values here
 
-            try
+   try
             {
                 rowsAffected = RunNonQueryProcedure(command);
                 if (rowsAffected == 1)
@@ -60,6 +70,65 @@ namespace MMABooksDB
                     mConnection.Close();
             }
         }
-         */
+
+        public bool Delete(IBaseProps props)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+public IBaseProps Create(IBaseProps p)
+{
+   int rowsAffected = 0;
+   CustomerProps props = (CustomerProps)p;
+
+   DBCommand command = new DBCommand();
+   command.CommandText = "usp_CustomerCreate";
+   command.CommandType = CommandType.StoredProcedure;
+   command.Parameters.Add("custId", DBDbType.Int32);
+   command.Parameters.Add("name_p", DBDbType.VarChar);
+   ... there are more parameters here
+   command.Parameters[0].Direction = ParameterDirection.Output;
+   command.Parameters["name_p"].Value = props.Name;
+   ... and more values here
+
+   try
+   {
+       rowsAffected = RunNonQueryProcedure(command);
+       if (rowsAffected == 1)
+       {
+           props.CustomerID = (int)command.Parameters[0].Value;
+           props.ConcurrencyID = 1;
+           return props;
+       }
+       else
+           throw new Exception("Unable to insert record. " + props.ToString());
+   }
+   catch (Exception e)
+   {
+       // log this error
+       throw;
+   }
+   finally
+   {
+       if (mConnection.State == ConnectionState.Open)
+           mConnection.Close();
+   }
+}
+*/
+        public IBaseProps Retrieve(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object RetrieveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(IBaseProps props)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
