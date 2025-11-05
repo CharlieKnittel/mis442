cec2aa90140ee45addd5acfd9dab65364230c761
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-11-05 15:18:46 -0800

    Created CRUD methods for CustomerDB class.

--- Lab 3/MMABooksFramework2022/MMABooksDB/CustomerDB.cs
+++ Lab 3/MMABooksFramework2022/MMABooksDB/CustomerDB.cs
@@ -112,17 +112,125 @@ namespace MMABooksDB
 
         public IBaseProps Retrieve(object key)
         {
-            throw new NotImplementedException();
+            DBDataReader data = null;
+            CustomerProps props = new CustomerProps();
+            DBCommand command = new DBCommand();
+
+            command.CommandText = "usp_CustomerSelect";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("custId", DBDbType.Int32);
+            command.Parameters["custId"].Value = key;
+
+            try
+            {
+                data = RunProcedure(command);
+                if (!data.IsClosed)
+                {
+                    if (data.Read())
+                    {
+                        props.SetState(data);
+                    }
+                    else
+                        throw new Exception("Record does not exist in the database.");
+                }
+                return props;
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (data != null)
+                {
+                    if (!data.IsClosed)
+                        data.Close();
+                }
+            }
         }
 
         public object RetrieveAll()
         {
-            throw new NotImplementedException();
+            List<CustomerProps> list = new List<CustomerProps>();
+            DBDataReader reader = null;
+            CustomerProps props;
+
+            try
+            {
+                reader = RunProcedure("usp_CustomerSelectAll");
+                if (!reader.IsClosed)
+                {
+                    while (reader.Read())
+                    {
+                        props = new CustomerProps();
+                        props.SetState(reader);
+                        list.Add(props);
+                    }
+                }
+                return list;
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (!reader.IsClosed)
+                {
+                    reader.Close();
+                }
+            }
         }
 
-        public bool Update(IBaseProps props)
+        public bool Update(IBaseProps p)
         {
-            throw new NotImplementedException();
+            int rowsAffected = 0;
+            CustomerProps props = (CustomerProps)p;
+
+            DBCommand command = new DBCommand();
+            command.CommandText = "usp_CustomerUpdate";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("custId", DBDbType.Int32);
+            command.Parameters.Add("name_p", DBDbType.VarChar);
+            command.Parameters.Add("address_p", DBDbType.VarChar);
+            command.Parameters.Add("city_p", DBDbType.VarChar);
+            command.Parameters.Add("state_p", DBDbType.VarChar);
+            command.Parameters.Add("zipCode_p", DBDbType.VarChar);
+            command.Parameters.Add("conCurrId", DBDbType.Int32);
+            command.Parameters["custId"].Value = props.CustomerID;
+            command.Parameters["name_p"].Value = props.Name;
+            command.Parameters["address_p"].Value = props.Address;
+            command.Parameters["city_p"].Value = props.City;
+            command.Parameters["state_p"].Value = props.State;
+            command.Parameters["zipCode_p"].Value = props.ZipCode;
+            command.Parameters["conCurrId"].Value = props.ConcurrencyID;
+
+            try
+            {
+                rowsAffected = RunNonQueryProcedure(command);
+                if (rowsAffected == 1)
+                {
+                    props.ConcurrencyID++;
+                    return true;
+                }
+                else
+                {
+                    string message = "Record cannot be updated. It has been edited by another user.";
+                    throw new Exception(message);
+                }
+            }
+            catch (Exception e)
+            {
+                // log this exception
+                throw;
+            }
+            finally
+            {
+                if (mConnection.State == ConnectionState.Open)
+                    mConnection.Close();
+            }
         }
     }
 }
