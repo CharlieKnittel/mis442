65b941dffab8e5765a2f7c3676f12dc414d4f1f5
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-12-18 15:50:11 -0800

    Created stored procedure to reset the database.

---
+++ Final Project/BITSFinal/BITSTests/IngredientTests.cs
@@ -0,0 +1,25 @@
+﻿using BITSEFClasses.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BITSTests
+{
+    public class IngredientTests
+    {
+        BITSContext dbContext;
+        Ingredient? i;
+        List<Ingredient>? ingredients;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbContext = new BITSContext();
+            dbContext.Database.ExecuteSqlRaw("call ")
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
