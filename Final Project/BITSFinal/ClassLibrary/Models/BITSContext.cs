5eeed1902bc2f29ac69ff455771b260808c3c887
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-12-18 15:28:29 -0800

    Fixed the OnCOnfiguring Event Handler.

--- Final Project/BITSFinal/ClassLibrary/Models/BITSContext.cs
+++ Final Project/BITSFinal/ClassLibrary/Models/BITSContext.cs
@@ -93,8 +93,14 @@ public partial class BITSContext : DbContext
     public virtual DbSet<Yeast> Yeasts { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=MIS4422025!;database=bits", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.43-mysql"));
+    {
+        string connectionString = ConfigDB.GetMySqlConnectionString();
+        if (!optionsBuilder.IsConfigured)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0));
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
