a46ad1bdf071bd498292f9f1b88151413e897de4
Mackenzie Knittel <mackenzie.knittel@oit.edu>
2025-12-18 16:06:35 -0800

    Created first unit test for Ingredient.

--- Final Project/BITSFinal/BITSTests/IngredientTests.cs
+++ Final Project/BITSFinal/BITSTests/IngredientTests.cs
@@ -1,5 +1,9 @@
 ﻿using BITSEFClasses.Models;
 using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BITSTests
 {
@@ -13,13 +17,24 @@ namespace BITSTests
         public void Setup()
         {
             dbContext = new BITSContext();
-            dbContext.Database.ExecuteSqlRaw("call ")
+            dbContext.Database.ExecuteSqlRaw("call usp_testingResetData()");
         }
 
         [Test]
-        public void Test1()
+        public void GetAllTest()
         {
-            Assert.Pass();
+            ingredients = dbContext.Ingredients.OrderBy(i => i.IngredientId).ToList();
+            Assert.AreEqual(1149, ingredients.Count);
+            Assert.AreEqual("Acid Malt", ingredients[0].Name);
+            PrintAll(ingredients);
+        }
+
+        public void PrintAll(List<Ingredient> ingredients)
+        {
+            foreach (Ingredient i in ingredients)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
